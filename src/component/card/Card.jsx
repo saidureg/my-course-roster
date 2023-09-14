5eb9ed38937961b8aa890be2a3574669107ec89a
saidureg <saidureg@gmail.com>
2023-09-14 23:42:57 +0600

    created side cart section where show course title and total & remaining credit hour and total price when user click on select button

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -7,21 +7,21 @@ const Card = ({ course }) => {
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
-        <img className="w-full px-3 pt-3" src={img} alt="Shoes" />
+        <img className="w-full px-3 pt-3" src={img} alt={title} />
       </figure>
       <div className="card-body space-y-2">
         <h2 className="card-title text-[#1C1B1B] font-semibold">{title}</h2>
-        <p className="text-[#1C1B1B99]">{details}</p>
+        <p className="text-[#1C1B1B99] text-sm">{details}</p>
         <div className="flex justify-between text-[#1C1B1B99] font-medium">
           <div className="flex items-center gap-5">
-            <p className="text-2xl">
+            <p className="text-xl lg:text-2xl">
               <FontAwesomeIcon icon={faDollarSign} />
             </p>
             <h4>Price: {price} </h4>
           </div>
 
           <div className="flex items-center gap-5">
-            <p className="text-2xl">
+            <p className="text-xl lg:text-2xl">
               <FontAwesomeIcon icon={faBookOpen} />
             </p>
             <h4>Credit: {credit_hour}hr </h4>
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -12,13 +12,28 @@ const Home = () => {
   }, []);
   return (
     <div className="flex flex-col lg:flex-row">
-      <div className="grid lg:grid-cols-3 gap-4 w-3/4 mx-auto lg:mx-0 p-5">
+      <div className="grid lg:grid-cols-3 gap-4 w-5/6 lg:mx-0 p-5">
         {loadData.map((course, idx) => (
           <Cards key={idx} course={course}></Cards>
         ))}
       </div>
-      <div>
-        <h3>Here add Bookmark section</h3>
+      <div className="mt-6 w-1/6 mx-4">
+        <div className="bg-base-100 rounded-xl space-y-4 p-5">
+          <p className="text-[#2F80ED] font-bold text-lg ">
+            Credit Hour Remaining{" "}
+          </p>
+          <hr />
+          <h3 className="text-[#1C1B1B] text-xl font-bold ">Course Name</h3>
+          <ul className="text-[#1C1B1B99] list-decimal pl-4">
+            <li> Introduction to c programming</li>
+          </ul>
+          <hr />
+          <p className="text-[#1C1B1BCC] font-medium ">Total Credit Hour : </p>
+          <hr />
+          <p className="text-[#1C1B1BCC] font-semibold">
+            Total Price : 48000 USD
+          </p>
+        </div>
       </div>
     </div>
   );
